2de15168bbad189c72977b51f5ca9c9ed1dce695
amgs <undisclosed>
2020-12-07 23:36:55 +0000

    Add thread to deal with client connections.

---
+++ src/pt/ual/sdp/MainNodeClient.java
@@ -0,0 +1,37 @@
+package pt.ual.sdp;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainNodeClient extends Thread {
+    private MainNode mainNode;
+    private int port;
+
+    public MainNodeClient(MainNode mainNode, int port) {
+        super();
+        this.mainNode = mainNode;
+        this.port = port;
+    }
+
+    @Override
+    public synchronized void start() {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            new MainNodeClientOperation(mainNode, socket).start();
+        }
+    }
+}
