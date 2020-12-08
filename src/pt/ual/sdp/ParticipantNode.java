1056297c92ae510c9bbd67b389f4e9aaf4e3fedc
amgs <undisclosed>
2020-12-08 01:03:24 +0000

    Add ParticipantNode response thread.

--- src/pt/ual/sdp/ParticipantNode.java
+++ src/pt/ual/sdp/ParticipantNode.java
@@ -2,18 +2,27 @@ package pt.ual.sdp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ParticipantNode {
     private int id;
+    private Map<String, String> database;
 
     public ParticipantNode(int id) {
         this.id = id;
+        this.database = new HashMap<>();
+    }
+
+    synchronized public Map<String, String> getDatabase() {
+        return database;
     }
 
     public static void main(String[] args) {
-        if(args.length != 3){
+        if (args.length != 3) {
             System.err.println("Wrong number of arguments");
             System.exit(1);
         }
@@ -22,16 +31,16 @@ public class ParticipantNode {
         int port = Integer.parseInt(args[2]);
 
         // Register with the MainNode
-        Socket socket = null;
+        Socket registrySocket = null;
         try {
-            socket = new Socket(mainNodeAddress, mainNodePort);
+            registrySocket = new Socket(mainNodeAddress, mainNodePort);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         PrintWriter printWriter = null;
         try {
-            printWriter = new PrintWriter(socket.getOutputStream());
+            printWriter = new PrintWriter(registrySocket.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,12 +49,31 @@ public class ParticipantNode {
 
         Scanner scanner = null;
         try {
-            scanner = new Scanner(socket.getInputStream());
+            scanner = new Scanner(registrySocket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
         int id = Integer.parseInt(scanner.nextLine());
-        System.out.println(id);
+        System.out.println("[Participant id: " + id + "]");
+
         ParticipantNode participantNode = new ParticipantNode(id);
+
+        // Accepts and respond to requests from the MainNode
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Socket socket = null;
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            new ParticipantNodeThread(participantNode, socket).start();
+        }
     }
 }
---
+++ src/pt/ual/sdp/ParticipantNodeThread.java
@@ -0,0 +1,37 @@
+package pt.ual.sdp;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ParticipantNodeThread extends Thread {
+    private ParticipantNode participantNode;
+    private Socket socket;
+
+    public ParticipantNodeThread(ParticipantNode participantNode, Socket socket) {
+        super();
+        this.participantNode = participantNode;
+        this.socket = socket;
+    }
+
+    @Override
+    public synchronized void start() {
+        Scanner scanner = SocketUtil.getScanner(socket);
+        PrintWriter printWriter = SocketUtil.getPrintWritter(socket);
+        String operation = scanner.nextLine();
+        String key, value;
+        switch (operation) {
+            case "R":
+                key = scanner.nextLine();
+                value = scanner.nextLine();
+                // TODO: check if the key should belong to this ParticipantNode.
+                participantNode.getDatabase().put(key, value);
+                printWriter.println("OK");
+                printWriter.flush();
+                break;
+            default:
+                break;
+        }
+        SocketUtil.closeSocket(socket);
+    }
+}
